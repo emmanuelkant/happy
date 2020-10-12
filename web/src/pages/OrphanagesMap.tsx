4fb24d72450f7c5d7ba6f3faeafe34981f4bb3c9
Emmanuel Kant Duarte <emmanuelkant.duarte@gmail.com>
2020-10-12 17:38:05 -0300

    add the routes and start the new page

---
+++ web/src/pages/OrphanagesMap.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+
+import mapMarkerImg from '../images/map-marker.svg';
+
+import '../styles/pages/orphanages-map.css';
+
+function OrphanagesMaps() {
+  return (
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Uma carinha feliz pintada de amarelo"/>
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
+        <footer>
+          <strong>Rio de Janeiro</strong>
+          <span>Rio de Janeiro</span>
+        </footer>
+
+        <Link to="" className="create-orphanage">
+          <FiPlus size="32" color="#fff" />
+        </Link>
+      </aside>
+    </div>
+  );
+}
+
+export default OrphanagesMaps;

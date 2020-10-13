eca38e8b5f69ec55820195e680a779688a4dc7fd
Emmanuel Kant Duarte <emmanuelkant.duarte@gmail.com>
2020-10-13 09:40:53 -0300

    add the migrations config and add the first table

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 
+import './database/connection';
+
 const app = express();
 
+app.use(express.json());
+
 app.get('/users', (request, response) => {
-  console.log('teste');
   return response.json({message: 'Hello World'})
 });
 

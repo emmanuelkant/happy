4844375f3b1cd7a5e2b496d34d0d6f109f929c61
Emmanuel Kant Duarte <emmanuelkant.duarte@gmail.com>
2020-10-13 21:29:38 -0300

    add views for orphanages and images

--- backend/src/controllers/orphanages-controller.ts
+++ backend/src/controllers/orphanages-controller.ts
@@ -1,18 +1,23 @@
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
 import Orphanage from '../models/Orphanage';
+import OrphanageView from '../views/orphanages_view';
 
 export default {
   async show(request: Request, response: Response) {
     const { id } = request.params;
     const orphanagesRepository = getRepository(Orphanage);
-    const orphanages = await orphanagesRepository.findOneOrFail(id);
-    return response.json(orphanages);
+    const orphanage = await orphanagesRepository.findOneOrFail(id, {
+      relations: ['images'],
+    });
+    return response.json(OrphanageView.render(orphanage));
   },
   async index(request: Request, response: Response) {
     const orphanagesRepository = getRepository(Orphanage);
-    const orphanages = await orphanagesRepository.find();
-    return response.json(orphanages);
+    const orphanages = await orphanagesRepository.find({
+      relations: ['images'],
+    });
+    return response.json(OrphanageView.renderMany(orphanages));
   },
   async create(request: Request, response: Response) {
     const {
